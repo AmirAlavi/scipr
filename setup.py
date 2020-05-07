75c2173ff0bb146165b05f8a6536e03a13377c40
Amir Alavi <s.amir.alavi@gmail.com>
2020-05-06 20:33:04 -0400

    Require py36 or above for f-strings

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ test_requirements = [ ]
 setup(
     author="Amir Alavi",
     author_email='amiralavi@cmu.edu',
-    python_requires='>=3.5',
+    python_requires='>=3.6',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
