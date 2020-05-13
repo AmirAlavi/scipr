d0a23c595eb75adaed450d68e3df60349e1943df
Amir Alavi <s.amir.alavi@gmail.com>
2020-05-13 01:45:13 -0400

    Bump version: 0.0.0 → 0.1.0

--- setup.py
+++ setup.py
@@ -42,6 +42,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/amiralavi/scipr',
-    version='0.0.0',
+    version='0.1.0',
     zip_safe=False,
 )
